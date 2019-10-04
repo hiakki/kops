70d9c45275089091922ab4bd59298ec5c0bfcb9c
hiakki <hy.akshay@gmail.com>
2019-10-04 07:51:11 +0000

    added automation.sh

---
+++ automation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ansible-playbook -i hosts installing_kubernetes.yml
+ansible-playbook -i hosts creating_user.yml
+sh configuring_master_node/configuring_master_node.sh
+sh configuring_worker_node/configuring_worker_node.sh
+ansible-playbook -i hosts joining_master_node.yml
+ansible-playbook -i hosts deploying_my_app.yml
