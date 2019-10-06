1f84108118f4e6ea224d4e1d6f8b58d4fff6e162
hiakki <hy.akshay@gmail.com>
2019-10-06 18:58:38 +0000

    new workers

--- configuring_worker_node/configuring_worker_node.sh
+++ configuring_worker_node/configuring_worker_node.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 n=1
-while [ $n -le $(cat kube-cluster/hosts | grep ' worker' -c) ]
+cd /root/kube-cluster
+while [ $n -le $(cat hosts | grep ' worker' -c) ]
 do 
 echo worker$n
   if [ $(ssh -o StrictHostKeyChecking=no kube@worker$n -p21 ls | grep -c tricksvibe) -lt 1 ]
   then
 
-    ansible-playbook -i kube-cluster/hosts --extra-vars "n=worker$n" kube-cluster/configuring_worker_node/configuring_worker_node.yml
+    ansible-playbook -i hosts --extra-vars "n=worker$n" configuring_worker_node/configuring_worker_node.yml
 
   fi
 
-  ansible-playbook -i kube-cluster/hosts --extra-vars "n=worker$n" kube-cluster/update_workers.yml
+  ansible-playbook -i hosts --extra-vars "n=worker$n" update_workers.yml
 
   n=$(( $n+1 ))
 done
