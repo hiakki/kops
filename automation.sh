3c3b4c1894a7214a239d1acf782a3a81cab15cae
hiakki <hy.akshay@gmail.com>
2019-10-08 03:48:14 +0000

    kops installation

--- automation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ansible-playbook -i hosts installing_kubernetes.yml
-ansible-playbook -i hosts creating_user.yml
-sh configuring_master_node/configuring_master_node.sh
-sh configuring_worker_node/configuring_worker_node.sh
-ansible-playbook -i hosts joining_master_node.yml
-ansible-playbook -i hosts restart_my_app.yml
